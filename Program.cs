8ff53270d7e58e5eebf07ae9b630cdf2e93bc05e
Anacarolinar21 <anacarolinarodriguescarolina2@gmail.com>
2022-03-04 17:59:45 -0300

    finalizar cidade

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Mongagua");
+Console.WriteLine("Peruibe");
+Console.WriteLine("Itanhaem");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Santos");
+Console.WriteLine("Sao Vicente");
